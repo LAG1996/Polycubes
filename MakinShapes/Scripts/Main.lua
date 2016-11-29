9721fe80b6272407a04002c310b7339d546de2bd
Luis Angel Garcia <luis.a.garcia01996@gmail.com>
2016-11-28 18:47:34 -0600

    Added Rotation to Three Squares

--- MakinShapes/Scripts/Main.lua
+++ MakinShapes/Scripts/Main.lua
@@ -14,17 +14,30 @@ squareMesh = SceneMesh.SceneMeshWithType(Mesh.QUAD_MESH)
 ]]--
 
 sceneEntity = SceneEntityInstance(scene, "Resources/scene.entity")
-sceneEntity:setColor(1, 0, 0, 1)
+sceneEntity:setColor(.6, .2, .4, 1)
 scene:addChild(sceneEntity)
 
+
 --[[
-##########Cube from mesh#################
+##########Squares from mesh#################
+]]--
+
+floater = Entity()
+floater:setPosition(0, 0, 0)
 squareMesh:getMesh():addVertex(.5, -.5, 0)
 squareMesh:getMesh():addVertex(.5, .5, 0)
 squareMesh:getMesh():addVertex(-.5, .5, 0)
 squareMesh:getMesh():addVertex(-.5, -.5, 0)
-
 squareMesh:setColor(1, 0, 0, 1)
+squareMesh:setAnchorPoint(Vector3(0.0, -1.0, 0.0))
+squareMesh:setPosition(.5, .5, 0)
+
+--Set an empty object as the parent of the square to act as the anchor point of the square.
+floater:addChild(squareMesh)
+floater:setPosition(0.0, 0.0, 0.0)
+squareMesh:setPosition(.5, 0, 0)
+
+
 
 backsquareMesh = SceneMesh.SceneMeshWithType(Mesh.QUAD_MESH)
 backsquareMesh:getMesh():addVertex(.5, -.5, 1)
@@ -34,9 +47,32 @@ backsquareMesh:getMesh():addVertex(-.5, -.5, 1)
 
 backsquareMesh:setColor(0, 0, 1, 1)
 
-scene:addChild(squareMesh)
+sideSquareMesh = SceneMesh.SceneMeshWithType(Mesh.QUAD_MESH)
+sideSquareMesh:getMesh():addVertex(.5, -.5, -1)
+sideSquareMesh:getMesh():addVertex(.5, .5, -1)
+sideSquareMesh:getMesh():addVertex(-.5, .5, -1)
+sideSquareMesh:getMesh():addVertex(-.5, -.5, -1)
+
+sideSquareMesh:setColor(0, 1, 0, 1)
+sideSquareMesh:setRoll(45)
+
+squareMesh.backfaceCulled = false
+backsquareMesh.backfaceCulled = false
+sideSquareMesh.backfaceCulled = false
+
+scene:addChild(floater)
 scene:addChild(backsquareMesh)
-]]--
+scene:addChild(sideSquareMesh)
+
 scene:setActiveCamera(scene:getDefaultCamera())
-scene:getDefaultCamera():setPosition(7, 0, 7)
+scene:getDefaultCamera():setPosition(0, 0, 10)
 scene:getDefaultCamera():lookAt(Vector3(0, 0, 0), Vector3(0, 1, 0))
+
+function Update(elapsed)
+	floater:setRoll(floater:getRoll() + .5) --Rotate the square's floater so that the square will seem to rotate over an anchor point
+	backsquareMesh:setPitch(backsquareMesh:getPitch() + 1)
+	sideSquareMesh:setYaw(sideSquareMesh:getYaw() + .7)
+	
+	sceneEntity:setPitch(sceneEntity:getPitch() + .3)
+	sceneEntity:setRoll(sceneEntity:getRoll() + .2)
+end
